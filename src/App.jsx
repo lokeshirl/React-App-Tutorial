7f670790b01a0ac9397c9f149e216815df439e7e
Lokesh <lokeshkumarjha07+github@gmail.com>
2023-05-11 06:20:46 +0530

    Basic setup of react-app-tut is done✅

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Video from "./components/Video";
+
+function App() {
+  return (
+    <>
+      <Video />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Video.jsx
@@ -0,0 +1,11 @@
+import "./Video.css";
+
+const Video = () => {
+  return (
+    <>
+      <h1>Hello from Video</h1>
+    </>
+  );
+};
+
+export default Video;
